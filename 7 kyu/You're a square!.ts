22e7b07d2c4be08dabcb06527b51eac5c9e08a8b
Vladimir Rukavishnikov <94602014+HungryVovka@users.noreply.github.com>
2023-12-13 23:47:33 +0500

    feat: 7 kyu solutions

--- 7 kyu/You're a square!.ts
+++ 7 kyu/You're a square!.ts
@@ -42,4 +42,8 @@ export default function isSquare(n: number): boolean {
 // (https://github.com/HungryVovka/Codewars-TypeScript)
 // 
 // License is GNU General Public License v3.0
+// (https://github.com/HungryVovka/Codewars-TypeScript/blob/main/LICENSE)
+// 
+// You should have received a copy of the GNU General Public License v3.0
+// along with this code. If not, see http://www.gnu.org/licenses/
 // -----------------------------------------------------------
